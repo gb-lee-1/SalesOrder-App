816c65e96f455bce888c32626bfd655c6d48eefb
GB-JELINK <gblee@jelink.co.kr>
2023-05-09 13:19:47 +0900

    first commit

---
+++ project1/README.md
@@ -0,0 +1,52 @@
+## Application Details
+|               |
+| ------------- |
+|**Generation Date and Time**<br>Tue May 09 2023 11:10:40 GMT+0900 (Korean Standard Time)|
+|**App Generator**<br>@sap/generator-fiori-freestyle|
+|**App Generator Version**<br>1.9.3|
+|**Generation Platform**<br>Visual Studio Code|
+|**Template Used**<br>2listdetail|
+|**Service Type**<br>SAP System (ABAP On Premise)|
+|**Service URL**<br>http://sapmento.mento.co.kr:8001//sap/opu/odata/sap/ZSALES_C00_SRV
+|**Module Name**<br>project1|
+|**Application Title**<br>App Title|
+|**Namespace**<br>|
+|**UI5 Theme**<br>sap_horizon|
+|**UI5 Version**<br>1.113.0|
+|**Enable Code Assist Libraries**<br>False|
+|**Enable TypeScript**<br>False|
+|**Add Eslint configuration**<br>False|
+|**Object collection**<br>SalesOrderSet|
+|**Object collection key**<br>Soid|
+|**Object ID**<br>Soid|
+|**Object number**<br>Amount|
+|**Object unit of measure**<br>Currency|
+|**entityType**<br>ToSalesOrderItem|
+|**entitySet**<br>SalesOrderItemSet|
+|**Line item collection key**<br>Soid|
+|**Line item ID**<br>Itemno|
+|**Line item number**<br>Quantity|
+
+## project1
+
+A Fiori application.
+
+### Starting the generated app
+
+-   This app has been generated using the SAP Fiori tools - App Generator, as part of the SAP Fiori tools suite.  In order to launch the generated app, simply run the following from the generated app root folder:
+
+```
+    npm start
+```
+
+- It is also possible to run the application using mock data that reflects the OData Service URL supplied during application generation.  In order to run the application with Mock Data, run the following from the generated app root folder:
+
+```
+    npm run start-mock
+```
+
+#### Pre-requisites:
+
+1. Active NodeJS LTS (Long Term Support) version and associated supported NPM version.  (See https://nodejs.org)
+
+
---
+++ project1/webapp/controller/DetailCreate.controller.js
@@ -0,0 +1,71 @@
+sap.ui.define([
+    "./BaseController",
+    "sap/ui/model/json/JSONModel",
+    "../model/formatter",
+    "sap/m/library"
+], function (BaseController, JSONModel, formatter, mobileLibrary) {
+    "use strict";
+
+    // shortcut for sap.m.URLHelper
+    var URLHelper = mobileLibrary.URLHelper;
+
+    return BaseController.extend("project1.controller.DetailCreate", {
+
+        formatter: formatter,
+
+        /* =========================================================== */
+        /* lifecycle methods                                           */
+        /* =========================================================== */
+
+        onInit: function () {
+            // Model used to manipulate control states. The chosen values make sure,
+            // detail page is busy indication immediately so there is no break in
+            // between the busy indication for loading the view's meta data
+            var oViewModel = new JSONModel({
+                busy : false,
+                delay : 0,
+                lineItemListTitle : this.getResourceBundle().getText("detailLineItemTableHeading")
+            });
+
+            this.getRouter().getRoute("objectCreate").attachPatternMatched(this._onObjectMatched, this);
+
+            this.setModel(oViewModel, "detailView");
+
+        },
+
+        /* =========================================================== */
+        /* event handlers                                              */
+        /* =========================================================== */
+
+        /**
+         * Event handler when the share by E-Mail button has been clicked
+         * @public
+         */
+        onSendEmailPress: function () {
+            var oViewModel = this.getModel("detailView");
+
+            URLHelper.triggerEmail(
+                null,
+                oViewModel.getProperty("/shareSendEmailSubject"),
+                oViewModel.getProperty("/shareSendEmailMessage")
+            );
+        },
+
+        /* =========================================================== */
+        /* begin: internal methods                                     */
+        /* =========================================================== */
+
+        /**
+         * Binds the view to the object path and expands the aggregated line items.
+         * @function
+         * @param {sap.ui.base.Event} oEvent pattern match event in route 'object'
+         * @private
+         */
+        _onObjectMatched: function (oEvent) {
+            
+            this.getModel("appView").setProperty("/layout", "TwoColumnsMidExpanded");
+            
+        }
+    });
+
+});
